588a78988b3a920ba51aaddee8c7d64d5ba77897
Max Fresonke <max@fresonke.com>
2016-10-06 11:41:24 -0400

    updated class example to include obejcts being passed value

--- Lab_03/class.cpp
+++ Lab_03/class.cpp
@@ -1,5 +1,25 @@
 #include "class.h"
 
+void modifyFooJK(Foo foo) {
+  foo.increment();
+}
+
+void modifyFooWithPtr(Foo *foo) {
+  foo->increment();
+}
+
+void modifyFooWithRef(Foo &foo) {
+  foo.increment();
+}
+
+int main() {
+  // create object by value
+  Foo foo(13);
+  modifyFooJK(foo);
+  modifyFooWithPtr(&foo);
+  modifyFooWithRef(foo);
+}
+
 Foo::Foo() {
 }
 
